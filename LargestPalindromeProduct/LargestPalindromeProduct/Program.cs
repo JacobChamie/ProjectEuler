da0221908f52690e0a4b2289b90f74847baf424f
programmer57 <jacobchamie@yahoo.com>
2019-09-09 07:55:52 -0700

    Added problem 4

---
+++ LargestPalindromeProduct/LargestPalindromeProduct/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LargestPalindromeProduct
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", Product(0,0));
+        }
+
+
+        private static Tuple<int, int> Product(int a, int b)
+        {
+            List<int> p = new List<int>();
+
+            int temp = 0;
+
+            var tuple = new Tuple<int, int>(0, 0);
+
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = 100; j < 1000; j++)
+                {
+                    int x = i * j;
+
+                    int r, sum = 0;
+
+                    temp = x;
+                    while (x > 0)
+                    {
+                        r = x % 10;
+                        sum = (sum * 10) + r;
+                        x /= 10;
+                    }
+
+                    if (temp == sum)
+                    {
+                        tuple = new Tuple<int, int>(i, j);
+                    }
+                }
+            }
+        
+            return tuple;
+        }
+    }
+}
