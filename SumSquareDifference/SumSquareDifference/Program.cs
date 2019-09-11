5917f3b01ae55a0e10ac6257f7804634823eed94
programmer57 <jacobchamie@yahoo.com>
2019-09-10 17:28:16 -0700

    Add files via upload

---
+++ SumSquareDifference/SumSquareDifference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SumSquareDifference
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", Square(100));
+        }
+
+
+        public static int Square(int range)
+        {
+            int sum = 0;
+            int square = 0;
+
+
+
+            //Find the sum of the squares
+            for (int i = 0; i < range; i++)
+            {
+                sum += i * i;
+            }
+
+
+            //Find the square of the sums
+            for (int j = 0; j < range; j++)
+            {
+                square += j;
+            }
+
+            square *= square;
+
+            return (square-sum);
+        }
+
+    }
+}
